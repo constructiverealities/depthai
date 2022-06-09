b91ecb1a325b8a1189e363237ebf4d4415750ae3
Justin Berger <j.david.berger@gmail.com>
2022-06-09 11:43:38 -0600

    Fix install issues for dependencies

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -1,5 +1,5 @@
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "63dc70c033e55edc8ea110132ca4637ca05bcd26")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "37ce724df72d73e26f2740b3b972c86fd1d22193")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
