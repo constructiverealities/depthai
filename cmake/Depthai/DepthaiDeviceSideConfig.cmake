2583d4d9e05ca355d13a8cbc7c3055a81e41f62b
Justin Berger <j.david.berger@gmail.com>
2022-06-14 00:29:13 -0600

    VGA adv filtering in FW

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -1,5 +1,5 @@
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "37ce724df72d73e26f2740b3b972c86fd1d22193")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "57768db7ed4de9c9f1241f8c0fe8b404b27e6d0e")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
