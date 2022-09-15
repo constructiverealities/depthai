b391b4c167fc5c0dde1d403c390554bc9b729480
Justin Berger <j.david.berger@gmail.com>
2022-09-14 21:43:30 -0600

    Update to fw

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -1,5 +1,5 @@
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "44d4c48ebc27eb575ac3539c4d929ced88a8e4d4")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "4444035a6c097324ee504b87633adeef42860056")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
