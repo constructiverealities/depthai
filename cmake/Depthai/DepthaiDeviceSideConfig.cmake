2f8bd2cc77c1fb7d6b46ef4d6d6907fa98dd56f6
Justin Berger <j.david.berger@gmail.com>
2022-12-14 21:24:20 -0700

    removed code that broke imu logic

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -1,5 +1,5 @@
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "ca80b149")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "0585d6716ca59a39d0c4d946045c5e54df3fb7b3")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
