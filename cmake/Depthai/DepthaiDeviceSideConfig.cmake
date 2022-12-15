06c57306776d9031b9f74a74c74d9febdd213013
Justin Berger <j.david.berger@gmail.com>
2022-12-14 18:03:54 -0700

    Write tof calib to spi flash if possible

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -1,5 +1,5 @@
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "4444035a6c097324ee504b87633adeef42860056")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "ca80b149")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
